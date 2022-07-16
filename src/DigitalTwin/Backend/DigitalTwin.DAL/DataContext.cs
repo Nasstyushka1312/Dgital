b3cf9c993ec3296ddb94991e5f1aa5b1489290ca
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-07-16 03:45:52 +0300

    Add user model

--- src/DigitalTwin/Backend/DigitalTwin.DAL/DataContext.cs
+++ src/DigitalTwin/Backend/DigitalTwin.DAL/DataContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DigitalTwin.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace DigitalTwin.DAL
 {
     public class DataContext : DbContext
     {
-        //public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
---
+++ src/DigitalTwin/Backend/DigitalTwin.DAL/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace DigitalTwin.DAL.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public bool IsIndividual { get; set; }
+    }
+}
